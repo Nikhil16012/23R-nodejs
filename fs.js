935c8b90142f406488b98a65c39a4ed23d5618c5
Nikhil16012 <nikhilreddy1616@gmail.com>
2025-01-30 12:24:48 +0530

    changes

--- fs.js
+++ fs.js
@@ -2,12 +2,22 @@
 var ss1={a:"nikhil",age:22}
 var a=['index122.js',"index23r.js","index24r.js"]
 for(i=0;i<a.length;i++){
+    fs.writeFile(`${a[i]}`,(err)=>{   
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("file has been created")
+    }
+    })
+    }
+for(i=0;i<a.length;i++){
 fs.unlink(`${a[i]}`,(err)=>{   
 if(err){
     console.log(err);
 }
 else{
-    console.log("file has been created")
+    console.log("file has been deleted")
 }
 })
 }
--- gmail1.js
+++ gmail1.js
@@ -1,9 +1,9 @@
-// let a=require("./gmail.js");
-// if(a.endsWith("@gmail.com")){
-//     console.log("email is vaild");
-// }
-// else{
-//     console.log("email is invalid");
-// }
+let a=require("./gmail.js");
+if(a.endsWith("@gmail.com")){
+    console.log("email is vaild");
+}
+else{
+    console.log("email is invalid");
+}
 
 
